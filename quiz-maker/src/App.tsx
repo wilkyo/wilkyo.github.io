5a34c7992564578502078a7e27fa894115610d2e
Willy FRANÇOIS <wfrancois45@gmail.com>
2023-11-29 15:20:11 +0100

    Ajout structure de base (#2)
    * Ajout structure de base
    
    * Ajout SASS
    
    * Ménage avant début
    
    ---------
    
    Co-authored-by: FRANCOIS Willy <willy.francois@soprasteria.com>

---
+++ quiz-maker/src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.scss";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <>
+      TODO
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </>
+  );
+}
+
+export default App;

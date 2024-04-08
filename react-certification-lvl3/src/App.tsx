6e02eacc6cf7c5597c9f21624f89edeb029628a6
FRANCOIS Willy <willy.francois@soprasteria.com>
2024-04-08 10:38:42 +0200

    Préparation du terrain

--- react-certification-lvl3/src/App.tsx
+++ react-certification-lvl3/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { HashRouter, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import { HomePage } from "./components/HomePage";
+import { LocalStorageHandlerPage } from "./components/LocalStorageHandlerPage";
+export const ROUTE_HOME = "/";
+export const ROUTE_LOCAL_STORAGE_HANDLER = "/local-storage-handler";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <HashRouter>
+      <Routes>
+        <Route path={ROUTE_HOME} element={<HomePage />} />
+        <Route
+          path={ROUTE_LOCAL_STORAGE_HANDLER}
+          element={<LocalStorageHandlerPage />}
+        />
+      </Routes>
+    </HashRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ react-certification-lvl3/src/components/LocalStorageHandlerPage.tsx
@@ -0,0 +1,14 @@
+import { FunctionComponent } from "react";
+import { MenuComponent } from "./MenuComponent";
+
+export const LocalStorageHandlerPage: FunctionComponent = () => {
+  return (
+    <div>
+      <h1>
+        EXERCISE #1 - Create a generic localStorage handler usable by React
+        function component
+      </h1>
+      <MenuComponent />
+    </div>
+  );
+};
